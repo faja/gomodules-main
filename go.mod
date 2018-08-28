d401795bc76b3bfff92f4a60552f9beef9b51deb
faja <mcabaj@gmail.com>
2018-08-28 12:12:40 +0200

    some tests

--- go.mod
+++ go.mod
@@ -1,1 +1,6 @@
 module github.com/faja/gomodules-main
+
+require (
+	github.com/faja/gomodules/p1 v1.0.5
+	github.com/faja/gomodules/p2 v1.0.4 // indirect
+)
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+import p1 "github.com/faja/gomodules/p1"
+import p2 "github.com/faja/gomodules/p2"
+
+func main() {
+	fmt.Println(p1.Func1())
+	fmt.Println(p1.Func2())
+	fmt.Println(p2.Func1())
+}
